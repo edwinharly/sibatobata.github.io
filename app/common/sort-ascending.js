8e9f56afbbc696cd978a7074e3013d2c7966afc8
Edwin <edwinharly@live.com>
2016-12-09 16:21:53 +0700

    Fitur sort (belum siap)

---
+++ app/common/sort-ascending.js
@@ -0,0 +1,10 @@
+'use strict';
+
+window.addEventListener('load', function() {
+	var ul = document.getElementById('ulTanaman').firstChild.innerHTML;
+	
+	console.log('test');
+	console.log(ul);
+});
+
+
