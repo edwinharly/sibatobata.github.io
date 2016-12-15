9b57d93955d83c4f71cb33b875c99ab32c547135
Edwin Harly <edwinharly@live.com>
2016-12-15 10:58:08 +0700

    API route, controller, handler artikel siap, tapi belum di tes

--- app/controllers/artikelController.client.js
+++ app/controllers/artikelController.client.js
@@ -1,9 +1,6 @@
 'use strict';
 
 (function () {
-
-    var ulArtikel = document.getElementById('ulArtikel');
-
     /*
     var profileId = document.querySelector('#profile-id') || null;
     var profileUsername = document.querySelector('#profile-username') || null;
@@ -11,12 +8,20 @@
     var displayName = document.querySelector('#display-name');
     */
     var apiUrl = appUrl + '/api/artikel';
-
+    var ulArtikel = document.getElementById('ulArtikel');
 
     ajaxFunctions.ready(ajaxFunctions.ajaxRequest('GET', apiUrl, function (data) {
         var articleObject = JSON.parse(data);
         // update elemen html dibawah
-        
+        var headline;
+        for (var i=0; i<articleObject.length; i++) {
+            headline = articleObject[i].headline;
+            var p = document.createElement('p');
+            var node = document.createTextNode(headline);
+            p.appendChild(node);
+            ulArtikel.appendChild(p);
+        }
+
 
     }));
 })();
