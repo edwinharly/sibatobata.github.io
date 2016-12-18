94c79cb29b9ccecb603f6187d335db984acb11a8
Edwin Harly <edwinharly@live.com>
2016-12-18 14:30:30 +0700

    Update progress fitur list tanaman

--- app/controllers/tanamanController.client.js
+++ app/controllers/tanamanController.client.js
@@ -3,10 +3,31 @@
 (function () {
 
     var ulTanaman = document.querySelector('#ulTanaman');
-    var apiUrl = appUrl + '/api/tanaman';
+    var apiUrl = appUrl + '/api/:id/tanaman';
+    var apiUrlUser = appUrl + '/api/:id';
 
-    ajaxFunctions.ready(ajaxFunctions.ajaxRequest('GET', apiUrl, function (data) {
+    function updateTanaman (data) {
+        var dbResult = JSON.parse(data);
+        //update elemen html dibawah
+        for (var i=0; i<dbResult.length; i++) {
+            var li = document.createElement('li');
 
-    }));
+            var aTanaman = document.createElement('a');
+            aTanaman.setAttribute('id', dbResult._id);
+            aTanaman.setAttribute('class', 'btn btn-default btn-lg');
+            aTanaman.setAttribute('href', '#');
+
+            var node = document.createTextNode(dbResult.nama);
+            aTanaman.appendChild(node);
+            li.appendChild(aTanaman);
+            ulTanaman.appendChild(li);
+        }
+
+    }
+
+    ajaxFunctions.ready(ajaxFunctions.ajaxRequest('GET', apiUrl, updateTanaman));
+
+    //event-event
+
 
 })();
